f00fd5d3bcbdd83c9c7e95b8dc69843be62adc68
Adrien <adrien.redondo@etu.unilim.fr>
2018-05-22 18:03:15 +0200

    introduction attribut vitesse classe vaisseau

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -3,6 +3,7 @@ package fr.unilim.iut.spaceinvaders;
 public class Vaisseau {
 	private Position origine;
     private Dimension dimension;
+    private int vitesse;
 
     public Vaisseau(int longueur, int hauteur) {
 	    this(longueur, hauteur, 0, 0);
@@ -15,6 +16,7 @@ public class Vaisseau {
     public Vaisseau(Dimension dimension, Position positionOrigine) {
 	    this.dimension = dimension;
 	    this.origine = positionOrigine;
+	    this.vitesse = 1;
     }
 
     public boolean occupeLaPosition(int x, int y) {
@@ -46,11 +48,11 @@ public class Vaisseau {
     }
 
     public void seDeplacerVersLaDroite() {
-	   	this.origine.changerAbscisse(this.origine.abscisse() + 1);
+	   	this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
     }
 
     public void seDeplacerVersLaGauche() {
-	   	this.origine.changerAbscisse(this.origine.abscisse() - 1);
+	   	this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
     }
 
     public void positionner(int x, int y) {
